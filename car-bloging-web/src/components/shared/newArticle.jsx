ed626d7cde18f3baaf4bf7191062024db7f642bb
muazawais <muazawais1947@gmail.com>
2025-08-11 21:35:15 +0500

    first react class14

--- car-bloging-web/src/components/shared/newArticle.jsx
+++ car-bloging-web/src/components/shared/newArticle.jsx
@@ -7,11 +7,12 @@ const NewArticle = ({ data }) => {
       <hr className="my-4 border-t border-gray-300" />
       {data.map((item) => (
         <div key={item.id} className="max-w-[1280px] mx-auto">
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4 w-[480px]">
             <img src={item.img} alt={item.title} />
           </div>
-          <div>
-            
+          <div className="w-[480px]">
+            <h2 className="text-xl font-bold">{item.title}</h2>
+            <p className="text-gray-600">{item.description}</p>
           </div>
         </div>
       ))}
--- react-class3/src/views/about.jsx
+++ react-class3/src/views/about.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function About() {
+const [input, setInput] = useState("")
+
   return (
-    <div>About</div>
+    <div>
+      <div className='flex flex-col m-12 gap-4'>
+      <label htmlFor='input' className='text-2xl font-bold'>Enter your name</label>
+      <input type='text' placeholder='Text' className=' p-2 border-solid border-2 border-black-500 rounded-md shadow-md ' />
+      <button className='bg-blue-500 text-white p-2 rounded-md shadow-md w-24 mt-4'>Submit</button>
+      </div>
+    </div>
   )
 }
 
