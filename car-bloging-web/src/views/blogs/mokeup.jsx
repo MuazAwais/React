aa0d329af0d0dab3aa088a92aa81d5736d91872f
muazawais <muazawais1947@gmail.com>
2025-08-17 22:11:31 +0500

    Project is Completed

--- car-bloging-web/src/views/blogs/mokeup.jsx
+++ car-bloging-web/src/views/blogs/mokeup.jsx
@@ -2,7 +2,8 @@ import { FiSend } from 'react-icons/fi'
 
 const Mokeup = () => {
   return (
-    <div className="relative max-w-[1280px] pt-[77px] pb-[36px] flex mx-auto text-white font-poppins bg-no-repeat bg-[#31323C]" >
+    <div className='bg-[#31323C]'>
+    <div className="relative max-w-[1280px] pt-[77px] pb-[36px] flex mx-auto text-white font-poppins bg-no-repeat " >
       <div className="flex flex-col  bg-cover bg-center pl-3 md:pl-11">
         <h2 className='font-bold text-[50px] leading-[1.2]  sm:text-[70px]'>Your Journey <br />Your Car <br />Your Way</h2>
         <p className='py-5 px-2 text-[14px] sm:text-[16px] leading-snug max-w-[400px]  sm:max-w-[650px]  text-justify'>Lorem ipsum dolor sit amet consectetur. Diam volutpat morbi elementum vel euismod aliquam. Amet ultrices neque augue consectetur purus phasellus. Ullamcorper lorem montes varius amet vestibulum tellus facilisis consequat pretium. Et faucibus laoreet molestie diam semper fames diam eget.
@@ -17,6 +18,7 @@ const Mokeup = () => {
         <img src="https://res.cloudinary.com/dv8dtipj1/image/upload/v1752951760/Rectangle_15_ibshhq.svg" alt="mokeup" className='hidden absolute left-[78%] top-[23%] z-2 lg:block lg:w-[180px] xl:w-[180px]' />
         <img src="https://res.cloudinary.com/dv8dtipj1/image/upload/v1752951750/Rectangle_14_tnspua.svg" alt="mokeup" className='hidden absolute left-[70%] top-[10%] z-5 lg:block lg:w-[180px] xl:w-[180px]' />
       </div>
+    </div>
     </div>
   )
 }
--- car-bloging-web/src/views/home/mokeup.jsx
+++ car-bloging-web/src/views/home/mokeup.jsx
@@ -2,7 +2,8 @@ import { FiSend } from 'react-icons/fi'
 
 const Mokeup = () => {
   return (
-    <div className=" max-w-[1280px] pt-[77px] pb-[36px] flex mx-auto text-white font-poppins bg-no-repeat bg-[#232536]" style={{ backgroundImage: `url(https://res.cloudinary.com/dv8dtipj1/image/upload/v1752998482/Group_9107_1_c3l1ty.svg)` }}>
+    <div className='bg-cover bg-center bg-no-repeat'style={{ backgroundImage: `url(https://res.cloudinary.com/dv8dtipj1/image/upload/v1752998482/Group_9107_1_c3l1ty.svg)` }}>
+    <div className=" max-w-[1280px] pt-[77px] pb-[36px] flex mx-auto text-white font-poppins bg-no-repeat" >
       <div className="flex flex-col  bg-cover bg-center pl-3 md:pl-11">
         <h2 className='font-bold text-[50px] leading-[1.2]  sm:text-[70px]'>Your Journey <br />Your Car <br />Your Way</h2>
         <p className='py-5 px-2 text-[14px] sm:text-[16px] leading-snug max-w-[400px]  sm:max-w-[650px]  text-justify'>Lorem ipsum dolor sit amet consectetur. Diam volutpat morbi elementum vel euismod aliquam. Amet ultrices neque augue consectetur purus phasellus. Ullamcorper lorem montes varius amet vestibulum tellus facilisis consequat pretium. Et faucibus laoreet molestie diam semper fames diam eget.
@@ -11,6 +12,7 @@ const Mokeup = () => {
             Subscribe <FiSend />
          </button>
       </div>
+    </div>
     </div>
   )
 }
